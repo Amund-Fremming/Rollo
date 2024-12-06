d77e6ea90b33823f4ad390cbf4284c06d4c0f8bc
Amund-fremming <amund.fremming@gmail.com>
2024-12-06 21:31:54 +0100

    Fuck

---
+++ infrastructure/GameUserList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+
+namespace infrastructure
+{
+    public interface IGameUserList
+    {
+        bool Add(string userId, string gameId);
+    }
+
+    public class GameUserList : IGameUserList
+    {
+        private ConcurrentDictionary<string, List<string>> map;
+
+        public GameUserList()
+        {
+            map = new ConcurrentDictionary<string, List<string>>();
+        }
+
+        public bool Add(string gameId, string userId)
+        {
+            var gameExists = map.TryGetValue(gameId, out _);
+            if (!gameExists)
+                return false;
+
+            map[gameId].Add(userId);
+            return true;
+        }
+
+        // Må ha noe for å fjerne dette galskapet
+    }
+}
---
+++ infrastructure/SpinHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace infrastructure
+{
+    public class SpinHub(IGameUserList gameUserList, ILogger<SpinHub> logger) : Hub
+    {
+        private const string STATE = nameof(STATE);
+        private const string MESSAGE = nameof(MESSAGE);
+
+        private readonly IGameUserList gameUserList = gameUserList;
+        private readonly ILogger<SpinHub> _logger = logger;
+
+        public async Task Subscribe(string userId, string gameId)
+        {
+            var success = gameUserList.Add(gameId, userId);
+            if (!success)
+            {
+                await Clients.Caller.SendAsync(MESSAGE, "Game does not exist.");
+                _logger.LogError("Game does not exist.");
+            }
+
+            if (success)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+                await Clients.Group(gameId).SendAsync(MESSAGE, "Joined game successfully.");
+                _logger.LogInformation("Joined game.");
+            }
+        }
+
+        //public async Task InitializeGame(string userId, string gameId)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        //public async Task StartSpin(string userId, string gameId)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
